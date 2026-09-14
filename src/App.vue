5dee47d9631970c58e8ef8dd75dd8ea1ac291c6c
heeyoon <hyiyunchoi@gmail.com>
2026-09-14 17:12:40 +0900

    Initial filmpath Project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="comtainer py-5">
+    <h1 class="text-primary">
+      서비스 이름
+    </h1>
+
+    <p class="lead">
+      테스트 서비스
+    </p>
+
+    <button class="btn btn-primary">
+      시작하기
+    </button>
+  </div>
+</template>
